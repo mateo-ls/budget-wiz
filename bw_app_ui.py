9d0205012fd84a90c0d15b851748a6fbbf334637
Aaron Georgeson <aaron.j.georgeson@biola.edu>
2022-02-18 14:46:03 -0800

    Added more UI elements; Organized UI elements in TransactionPage

--- bw_app_ui.py
+++ bw_app_ui.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 from tkinter import font as tkfont
 from tkinter import *
-# Need to import and setup mySQL database
+from tkcalendar import Calendar, DateEntry
+from PIL import Image, ImageTk
+import mysql.connector
 
 
 class MainView(tk.Tk):
@@ -38,20 +40,57 @@ class TransactionPage(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         
+
+        # Establishes this pages UI elements/widgets
+
+        # Buttons
         selfButton = tk.Button(self, text="Transactions")
         analyticsButton = tk.Button(self, text="Analytics")
         addButton = tk.Button(self, text="Add", command=lambda: controller.show_frame("AddTransactionPage"))
         editButton = tk.Button(self, text="Edit")
         deleteButton = tk.Button(self, text="Delete")
+        
+        # Image Buttons (Left and Right Arrows)
+        # Getting this to work was really dumb
+        # Let's stay away from image UI elements
+        arrowImage = Image.open('resources\\arrow_icon.png')
+        arrowImage = arrowImage.resize((30, 30), Image.ANTIALIAS)
+        arrowImageFlipped = Image.open('resources\\arrow_icon_flipped.png')
+        arrowImageFlipped = arrowImageFlipped.resize((30, 30), Image.ANTIALIAS)
+        arrowIcon = ImageTk.PhotoImage(arrowImage)
+        arrowIconFlipped = ImageTk.PhotoImage(arrowImageFlipped)
+        leftArrowButton = tk.Button(self, image=arrowIcon, borderwidth=0)
+        leftArrowButton.image = arrowIcon
+        rightArrowButton = tk.Button(self, image=arrowIconFlipped, borderwidth=0)
+        rightArrowButton.image = arrowIconFlipped
 
+        # Labels (text)
+        label = tk.Label(self, text="This is Transaction Page")
+
+
+        # Establishes layout of above elements
+        # Buttons
         selfButton.grid(row=0, column=0)
         analyticsButton.grid(row=0, column=1)
-        deleteButton.grid(row=0, column=10)
-        editButton.grid(row=0, column=9)
-        addButton.grid(row=0, column=8)
+        deleteButton.grid(row=0, column=7)
+        editButton.grid(row=0, column=6)
+        addButton.grid(row=0, column=5)
 
-        label = tk.Label(self, text="This is Transaction Page")
+        # Image Buttons
+        leftArrowButton.grid(row=1, column=1)
+        rightArrowButton.grid(row=1, column=3)
+
+        #Labels
         label.grid(row=5, column=5)
+
+
+        # Sets minimun sizes for all columns or rows
+        # Might want to change/get rid of this later
+        columnCount, rowCount = self.grid_size()
+        for column in range(columnCount):
+            self.grid_columnconfigure(column, minsize=100)
+        for row in range(rowCount):
+            self.grid_rowconfigure(row, minsize=100)
        
 
 class AddTransactionPage(tk.Frame):
@@ -78,6 +117,17 @@ class AnalyticsPage(tk.Frame):
         self.controller = controller
         label = tk.Label(self, text="This is Analytics Page")
         label.pack(side="top", fill="both", expand=True)
+
+        arrowImage = Image.open('resources\\arrow_icon.png')
+        arrowImage = arrowImage.resize((30, 30), Image.ANTIALIAS)
+        arrowImageFlipped = Image.open('resources\\arrow_icon_flipped.png')
+        arrowImageFlipped = arrowImageFlipped.resize((30, 30), Image.ANTIALIAS)
+        arrowIcon = ImageTk.PhotoImage(arrowImage)
+        arrowIconFlipped = ImageTk.PhotoImage(arrowImageFlipped)
+        leftArrowButton = tk.Button(self, image=arrowIcon, borderwidth=0)
+        leftArrowButton.image = arrowIcon
+        rightArrowButton = tk.Button(self, image=arrowIconFlipped, borderwidth=0)
+        rightArrowButton.image = arrowIconFlipped
         
 
 if __name__ == "__main__":
