9c95c251cccc6e2f231380384aae3783ffb7fd21
Aaron Georgeson <aaron.j.georgeson@biola.edu>
2022-02-18 18:29:12 -0800

    Add Incomes and Expenses tableviews; partial implementation of LoadIncomes and LoadExpenses

--- bw_app_ui.py
+++ bw_app_ui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 from tkinter import font as tkfont
 from tkinter import *
 from tkcalendar import Calendar, DateEntry
@@ -41,7 +42,8 @@ class TransactionPage(tk.Frame):
         self.controller = controller
         
 
-        # Establishes this pages UI elements/widgets
+        # Establishes this pages UI elements/
+        # Date, description, amount, category
 
         # Buttons
         selfButton = tk.Button(self, text="Transactions")
@@ -71,34 +73,134 @@ class TransactionPage(tk.Frame):
         expenseLabel = tk.Label(self, text="Expenses")
         label = tk.Label(self, text="This is Transaction Page")
 
+        # Incomes Treeview
+        columns = ("#1", "#2", "#3", "#4")
+        self.tvIncomes = ttk.Treeview(self, show="headings", height="5", columns=columns)
+        # Do we want a column for TransactionID?
+        self.tvIncomes.heading("#1", text="Date", anchor="center")
+        self.tvIncomes.column("#1", width=80, anchor="center",stretch=True)
+        self.tvIncomes.heading("#2", text="Description", anchor="center")
+        self.tvIncomes.column("#2", width=80, anchor="center",stretch=True)
+        self.tvIncomes.heading("#3", text="Amount", anchor="center")
+        self.tvIncomes.column("#3", width=80, anchor="center",stretch=True)
+        self.tvIncomes.heading("#4", text="Category", anchor="center")
+        self.tvIncomes.column("#4", width=80, anchor="center",stretch=True)
+
+        vsb = ttk.Scrollbar(self, orient=tk.VERTICAL, command=self.tvIncomes.yview)
+        # TODO Need to place vertical scroll bar using either grid or place
+        # We'll figure that out later
+        self.tvIncomes.configure(yscroll=vsb.set)
+        self.tvIncomes.bind("<<TreeviewSelect>>", self.showSelectedRecordIncome)
+
+        # Expenses Treeview
+        columns = ("#1", "#2", "#3", "#4")
+        self.tvExpenses = ttk.Treeview(self, show="headings", height="5", columns=columns)
+        # Do we want a column for TransactionID?
+        self.tvExpenses.heading("#1", text="Date", anchor="center")
+        self.tvExpenses.column("#1", width=80, anchor="center",stretch=True)
+        self.tvExpenses.heading("#2", text="Description", anchor="center")
+        self.tvExpenses.column("#2", width=80, anchor="center",stretch=True)
+        self.tvExpenses.heading("#3", text="Amount", anchor="center")
+        self.tvExpenses.column("#3", width=80, anchor="center",stretch=True)
+        self.tvExpenses.heading("#4", text="Category", anchor="center")
+        self.tvExpenses.column("#4", width=80, anchor="center",stretch=True)
+
+        vsb = ttk.Scrollbar(self, orient=tk.VERTICAL, command=self.tvExpenses.yview)
+        # TODO Need to place vertical scroll bar using either grid or place
+        # We'll figure that out later
+        self.tvExpenses.configure(yscroll=vsb.set)
+        self.tvExpenses.bind("<<TreeviewSelect>>", self.showSelectedRecordExpense)
+
 
         # Establishes layout of above elements
+        # This is bad
+        # TODO make it pretty
         # Buttons
         selfButton.grid(row=0, column=0)
         analyticsButton.grid(row=0, column=1)
         deleteButton.grid(row=0, column=7)
         editButton.grid(row=0, column=6)
         addButton.grid(row=0, column=5)
-        thisMonthButton.grid(row=1, column=4)
+        thisMonthButton.grid(row=1, column=5)
 
         # Image Buttons
         leftArrowButton.grid(row=1, column=1)
         rightArrowButton.grid(row=1, column=3)
 
-        #Labels
+        # Labels
         selectedMonthLabel.grid(row=1, column=2)
         incomeLabel.grid(row=2, column=2)
         expenseLabel.grid(row=2, column=5)
         label.grid(row=5, column=5)
 
+        # Treeviews
+        self.tvIncomes.grid(row=3, column=1, columnspan=3)
+        self.tvExpenses.grid(row=3, column=5, columnspan=3)
+
+        self.LoadIncomes()
+        self.LoadExpenses()
+
 
         # Sets minimun sizes for all columns or rows
         # Might want to change/get rid of this later
-        columnCount, rowCount = self.grid_size()
-        for column in range(columnCount):
-            self.grid_columnconfigure(column, minsize=100)
-        for row in range(rowCount):
-            self.grid_rowconfigure(row, minsize=100)
+        #columnCount, rowCount = self.grid_size()
+        #for column in range(columnCount):
+        #    self.grid_columnconfigure(column, minsize=100)
+        #for row in range(rowCount):
+        #    self.grid_rowconfigure(row, minsize=100)
+    
+
+    def showSelectedRecordIncome(self, event):
+        global transactionID
+        transactionID = self.tvIncomes.selection()[0]
+        return transactionID
+    
+    def showSelectedRecordExpense(self, event):
+        global transactionID
+        transactionID = self.tvExpenses.selection()[0]
+        return transactionID
+
+    
+    # When called, loads Income data from database into tvIncomes
+    def LoadIncomes(self):
+        # TODO Ensure connection to database here
+        # Clears the treeview tvIncomes
+        self.tvIncomes.delete(*self.tvIncomes.get_children())
+        # TODO Execute SQL query here
+        rows = []#db_cursor.fetchall()
+        TransactionID = ""
+        Date = ""
+        Description = ""
+        Amount = ""
+        Category = ""
+        for row in rows:
+            TransactionID = row[0]
+            Date = row[1]
+            Description = row[2]
+            Amount = row[3]
+            Category = row[4]
+        self.tvIncomes.insert("", 'end', text=TransactionID, values=(Date, Description, Amount, Category))
+
+    # When called, loads Expense data from database into tvExpenses
+    def LoadExpenses(self):
+        # TODO Ensure connection to database here
+        # Clears the treeview tvExpenses
+        self.tvExpenses.delete(*self.tvExpenses.get_children())
+        # TODO Execute SQL query here
+        rows = []#db_cursor.fetchall()
+        TransactionID = ""
+        Date = ""
+        Description = ""
+        Amount = ""
+        Category = ""
+        for row in rows:
+            TransactionID[0]
+            Date = row[1]
+            Description = row[2]
+            Amount = row[3]
+            Category = row[4]
+        self.tvExpenses.insert("", 'end', text=TransactionID, values=(Date, Description, Amount, Category))
+
        
 
 class AddTransactionPage(tk.Frame):
