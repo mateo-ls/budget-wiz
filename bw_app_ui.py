0cbd6f1e1daad95694daaf15cf25e1d8892da384
AaronGeorgeson <72892772+AaronGeorgeson@users.noreply.github.com>
2022-03-30 10:47:56 -0700

    Analytics page UI elements

--- bw_app_ui.py
+++ bw_app_ui.py
@@ -615,31 +615,75 @@ class AnalyticsPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        #label.pack(side="top", fill="both", expand=True)
-
-        transactionsButton = tk.Button(self, text="Back to Transactions", command=lambda: controller.show_frame("TransactionPage"))
-        
-        # Labels
-        label = tk.Label(self, text="This is Analytics Page")
-
-        ## Layout of above UI elements ##
 
         # Buttons
-        transactionsButton.grid(row=0, column=0)
+        selfButton = tk.Button(
+            self, 
+            text="Transactions", 
+            command=lambda: controller.show_frame("TransactionPage"))
+        analyticsButton = tk.Button(self, text="Analytics")
 
-        # Labels
-        label.grid(row=1, column=0)
-
+        thisMonthButton = tk.Button(self, text="This Month")
+        
         arrowImage = Image.open('resources/arrow_icon.png')
         arrowImage = arrowImage.resize((30, 30), Image.ANTIALIAS)
         arrowImageFlipped = Image.open('resources/arrow_icon_flipped.png')
         arrowImageFlipped = arrowImageFlipped.resize((30, 30), Image.ANTIALIAS)
         arrowIcon = ImageTk.PhotoImage(arrowImage)
         arrowIconFlipped = ImageTk.PhotoImage(arrowImageFlipped)
-        leftArrowButton = tk.Button(self, image=arrowIcon, borderwidth=0)
+
+
+        # ----- Month Selection Buttons and Labels ----- 
+        month_and_year = f"{current_date.strftime('%B')} {current_date.strftime('%Y')}"
+        self.selectedMonthLabel = tk.Label(self, text=month_and_year)
+
+        leftArrowButton = tk.Button(
+            self,
+            image=arrowIcon,
+            borderwidth=0,
+            command=lambda: self.changeMonth("left")
+        )
         leftArrowButton.image = arrowIcon
-        rightArrowButton = tk.Button(self, image=arrowIconFlipped, borderwidth=0)
+
+        rightArrowButton = tk.Button(
+            self, 
+            image=arrowIconFlipped, 
+            borderwidth=0,
+            command=lambda: self.changeMonth("right")
+        )
         rightArrowButton.image = arrowIconFlipped
+
+        # Labels
+
+        # Categories Treeview
+        columns = ("#1", "#2")
+        self.tvCategoryTotals = ttk.Treeview(self, show="headings", height="5", columns=columns)
+        self.tvCategoryTotals.heading("#1", text="Category", anchor="center")
+        self.tvCategoryTotals.column("#1", width=80, anchor="center", stretch=True)
+        self.tvCategoryTotals.heading("#2", text="Amount", anchor="center")
+        self.tvCategoryTotals.column("#2", width=80, anchor="center", stretch=True)
+        
+        vsb = ttk.Scrollbar(self, orient=tk.VERTICAL, command=self.tvCategoryTotals.yview)
+        self.tvCategoryTotals.configure(yscroll=vsb.set)
+        # Don't know if we need to select anything for this treeview
+        # self.tvCategoryTotals.bind("<<TreeviewSelect>>", s)
+
+        ## Layout
+
+        # Buttons
+        selfButton.grid(row=0, column=0)
+        analyticsButton.grid(row=0, column=1)
+        thisMonthButton.grid(row=1, column=5)
+
+        # Image Buttons
+        leftArrowButton.grid(row=1, column=1)
+        rightArrowButton.grid(row=1, column=3)
+
+        # Labels
+        self.selectedMonthLabel.grid(row=1, column=2)
+
+        # Treeviews
+        self.tvCategoryTotals.grid(row=3, column=1, columnspan=3)
         
 
 if __name__ == "__main__":
